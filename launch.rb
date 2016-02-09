254cc3fba778e8ee835a089d30ee3db5a7116876
Russell Osborne <russell@theironyard.com>
2016-02-09 16:54:22 -0500

    change default connection style

--- launch.rb
+++ launch.rb
@@ -1,10 +1,7 @@
 require './setup'
 
 # Optional parameters :host, :port, otherwise defaults will be used (127.0.0.1:8085)
-link = Kuby::Link.new
+link = Kuby::Link.new(host: '****.ngrok.com', port: 80)
 
 # Connect to KSP
 link.connect!
-
-# Launch Console
-binding.pry
